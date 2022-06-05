a93bd72c0310ce789a0a722d52d0deb2078fce87
pikaty4 <71702322+pikaty4@users.noreply.github.com>
2022-06-05 16:44:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,8 @@ std::string infx2pstfx(std::string inpt) {
             //res += " ";
         } else if (inpt[i] == '(') {
             stack1.push(inpt[i]);
-        } else if ((inpt[i] == '+' || inpt[i] == '-') && (stack1.get() != '/') &&
+        } else if ((inpt[i] == '+' || inpt[i] == '-') &&
+                   (stack1.get() != '/') &&
                    (stack1.get() != '*') && (stack1.get() != '+') &&
             (stack1.get() != '-')) {
             stack1.push(inpt[i]);
