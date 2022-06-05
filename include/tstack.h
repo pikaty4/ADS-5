80345ee8af174c6f66db3594412125b7e569bbeb
pikaty4 <71702322+pikaty4@users.noreply.github.com>
2022-06-05 16:48:27 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,30 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <string>
+
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T arr[50];
+    int top;
+ public:
+    TStack() :top(-1) { }
+    T get() const {
+        return arr[top];
+    }
+    void pop() {
+        if (top >= 0) top--;
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    void push(T item) {
+        if (top < size - 1) arr[++top] = item;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
